aa8ce09fe2afd3b28b83ba2baf7d80797e2e2f00
sharif <shahanur700@gmail.com>
2020-12-09 13:19:39 +0600

    drag and drop

--- pages/index.vue
+++ pages/index.vue
@@ -1,89 +1,60 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
+<v-container>
+  <v-row>
+    <no-ssr>
+      <draggable class="row">
+        <v-col md="3" cols="12">
+          <v-card>
+            <v-card-title>card 1</v-card-title>
+            <v-card-text>I am 1</v-card-text>
+          </v-card>
+        </v-col>
+        <v-col md="3" cols="12">
+          <v-card>
+            <v-card-title>card 2</v-card-title>
+            <v-card-text>I am 2</v-card-text>
+          </v-card>
+        </v-col>
+      </draggable>
+    </no-ssr>
   </v-row>
+</v-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  display: "Two Lists",
+  order: 1,
+  data:()=>({
+    list1: [
+      { name: "John", id: 1 },
+      { name: "Joao", id: 2 },
+      { name: "Jean", id: 3 },
+      { name: "Gerard", id: 4 }
+    ],
+    list2: [
+      { name: "Juan", id: 5 },
+      { name: "Edgard", id: 6 },
+      { name: "Johnson", id: 7 }
+    ]
+  }),
+  methods:{
+    add: function() {
+      this.list.push({ name: "Juan" });
+    },
+    replace: function() {
+      this.list = [{ name: "Edgard" }];
+    },
+    clone: function(el) {
+      return {
+        name: el.name + " cloned"
+      };
+    },
+    log: function(evt) {
+      window.console.log(evt);
+    }
   }
 }
 </script>
